e7f99fc6e426faf1b63b1c796e71d987004bc146
sethrimer3 <sethrimer3@gmail.com>
2026-05-31 23:42:07 -0600

    asd

--- run-desktop.bat
+++ run-desktop.bat
@@ -10,7 +10,9 @@ if not exist "node_modules\" (
   call npm install
   if errorlevel 1 goto error
 )
-call npm run desktop
+call npm run build
+if errorlevel 1 goto error
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Add-Type -Name Win32Window -Namespace TinyBaseIdle -MemberDefinition '[System.Runtime.InteropServices.DllImport(\"kernel32.dll\")] public static extern System.IntPtr GetConsoleWindow(); [System.Runtime.InteropServices.DllImport(\"user32.dll\")] public static extern bool ShowWindow(System.IntPtr hWnd, int nCmdShow);'; $electron = Start-Process -FilePath 'node_modules\\.bin\\electron.cmd' -ArgumentList @('.') -WorkingDirectory (Get-Location).Path -PassThru; Start-Sleep -Seconds 1; [TinyBaseIdle.Win32Window]::ShowWindow([TinyBaseIdle.Win32Window]::GetConsoleWindow(), 0) | Out-Null; Wait-Process -Id $electron.Id; exit $electron.ExitCode"
 if errorlevel 1 goto error
 popd
 exit /b 0
